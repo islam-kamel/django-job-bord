4e09fbadaa7e6be2fb117a010703a52ca1ef9441
islam-kamel <islam.kamel@agr.svu.edu.eg>
2020-08-14 19:21:57 +0200

    add media files & static & templates install pillow lib

--- job/models.py
+++ job/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 JOB_TYPE = (
     ('Full Time', 'Full Time'),
-    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
 )
+
+
+def image_upload(instance, filename):
+    ext = filename.split('.')
+    return f'jobs/{str(instance.id) + "." + ext[-1]}'
 
 
 class Job(models.Model):  # table
@@ -16,6 +21,7 @@ class Job(models.Model):  # table
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload, default='default.png')
 
     def __str__(self):
         return self.title
